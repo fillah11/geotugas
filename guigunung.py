4cd07b94463bf50bf0d664992dd54f675aee08e2
Fillah Abdul Quddus <fillahaq@gmail.com>
2023-06-10 17:57:19 +0700

    final

--- guigunung.py
+++ guigunung.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+from tkinter import font
 import base64
 import os
 import sys
@@ -13,7 +14,11 @@ def on_button1_click():
     elif selected_option == "Option 3":
         label.config(image=resized_image7)
     elif selected_option == "Option 4":
-        label.config(image=resized_image7)
+        label.config(image=resized_image10)
+    elif selected_option == "Option 5":
+        label.config(image=resized_image13)
+    elif selected_option == "Option 6":
+        label.config(image=resized_image16)
     else:
         label.config(image=placeholder_image)
 
@@ -26,7 +31,11 @@ def on_button2_click():
     elif selected_option == "Option 3":
         label.config(image=resized_image8)
     elif selected_option == "Option 4":
-        label.config(image=resized_image7)
+        label.config(image=resized_image11)
+    elif selected_option == "Option 5":
+        label.config(image=resized_image14)
+    elif selected_option == "Option 6":
+        label.config(image=resized_image17)
     else:
         label.config(image=placeholder_image)
 
@@ -39,12 +48,18 @@ def on_button3_click():
     elif selected_option == "Option 3":
         label.config(image=resized_image9)
     elif selected_option == "Option 4":
-        label.config(image=resized_image7)
+        label.config(image=resized_image12)
+    elif selected_option == "Option 5":
+        label.config(image=resized_image15)
+    elif selected_option == "Option 6":
+        label.config(image=resized_image18)
     else:
         label.config(image=placeholder_image)
 
 # Membuat jendela utama
 window = tk.Tk()
+
+font_style = font.Font(weight="bold")
 
 # Variabel terkait radio button
 radio_var = tk.StringVar()
@@ -68,7 +83,16 @@ image5_path = os.path.join(image_dir, "kontursumbing.png")
 image6_path = os.path.join(image_dir, "plotsumbing.png")
 image7_path = os.path.join(image_dir, "gunungcikurai.png")
 image8_path = os.path.join(image_dir, "konturcikuray.png")
-image9_path = os.path.join(image_dir, "cikurayplot.png")
+image9_path = os.path.join(image_dir, "plotcikuray.png")
+image10_path = os.path.join(image_dir, "gambarsuket.png")
+image11_path = os.path.join(image_dir, "kontursuket.png")
+image12_path = os.path.join(image_dir, "plotsuket.png")
+image13_path = os.path.join(image_dir, "gambarlawu.png")
+image14_path = os.path.join(image_dir, "gambarlawu.png")
+image15_path = os.path.join(image_dir, "plotlawu.png")
+image16_path = os.path.join(image_dir, "gambarsindoro.png")
+image17_path = os.path.join(image_dir, "gambarsindoro.png")
+image18_path = os.path.join(image_dir, "plotsindoro.png")
 
 # Membaca dan mengkonversi gambar menjadi data base64
 with open(image1_path, "rb") as image_file:
@@ -89,6 +113,24 @@ with open(image8_path, "rb") as image_file:
     image8_data = base64.b64encode(image_file.read()).decode("utf-8")
 with open(image9_path, "rb") as image_file:
     image9_data = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image10_path, "rb") as image_file:
+    image10_data = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image11_path, "rb") as image_file:
+    image11_data = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image12_path, "rb") as image_file:
+    image12_data = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image13_path, "rb") as image_file:
+    image13_data = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image14_path, "rb") as image_file:
+    image14_data = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image15_path, "rb") as image_file:
+    image15_data = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image16_path, "rb") as image_file:
+    image16_data = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image17_path, "rb") as image_file:
+    image17_data = base64.b64encode(image_file.read()).decode("utf-8")
+with open(image18_path, "rb") as image_file:
+    image18_data = base64.b64encode(image_file.read()).decode("utf-8")
 
 # Membuat objek ImageTk dari gambar yang sudah diubah ukurannya
 resized_image1 = ImageTk.PhotoImage(Image.open(image1_path).resize(new_size))
@@ -100,6 +142,15 @@ resized_image6 = ImageTk.PhotoImage(Image.open(image6_path).resize(new_size))
 resized_image7 = ImageTk.PhotoImage(Image.open(image7_path).resize(new_size))
 resized_image8 = ImageTk.PhotoImage(Image.open(image8_path).resize(new_size))
 resized_image9 = ImageTk.PhotoImage(Image.open(image9_path).resize(new_size))
+resized_image10 = ImageTk.PhotoImage(Image.open(image10_path).resize(new_size))
+resized_image11 = ImageTk.PhotoImage(Image.open(image11_path).resize(new_size))
+resized_image12 = ImageTk.PhotoImage(Image.open(image12_path).resize(new_size))
+resized_image13 = ImageTk.PhotoImage(Image.open(image13_path).resize(new_size))
+resized_image14 = ImageTk.PhotoImage(Image.open(image14_path).resize(new_size))
+resized_image15 = ImageTk.PhotoImage(Image.open(image15_path).resize(new_size))
+resized_image16 = ImageTk.PhotoImage(Image.open(image16_path).resize(new_size))
+resized_image17 = ImageTk.PhotoImage(Image.open(image17_path).resize(new_size))
+resized_image18 = ImageTk.PhotoImage(Image.open(image18_path).resize(new_size))
 placeholder_image = ImageTk.PhotoImage(Image.open("GAMBAR_ASLI.png").resize(new_size))
 
 # Judul
@@ -108,16 +159,29 @@ title_label.config(highlightbackground="yellow", highlightcolor="yellow")
 title_label.pack(pady=10)
 
 # Membuat radio button
-radio_button1 = tk.Radiobutton(frame, text="Merbabu", variable=radio_var, value="Option 1", bg="yellow", fg="red")
-radio_button2 = tk.Radiobutton(frame, text="Sumbing", variable=radio_var, value="Option 2", bg="yellow", fg="red")
-radio_button3 = tk.Radiobutton(frame, text="Cikuray", variable=radio_var, value="Option 3", bg="yellow", fg="red")
-radio_button4 = tk.Radiobutton(frame, text="Option 4", variable=radio_var, value="Option 4", bg="yellow", fg="red")
+radio_button1 = tk.Radiobutton(frame, text="Merbabu", variable=radio_var, value="Option 1", bg="yellow", fg="red",font=font_style)
+radio_button2 = tk.Radiobutton(frame, text="Sumbing", variable=radio_var, value="Option 2", bg="yellow", fg="red",font=font_style)
+radio_button3 = tk.Radiobutton(frame, text="Cikuray", variable=radio_var, value="Option 3", bg="yellow", fg="red",font=font_style)
+radio_button4 = tk.Radiobutton(frame, text="Suket", variable=radio_var, value="Option 4", bg="yellow", fg="red",font=font_style)
+radio_button5 = tk.Radiobutton(frame, text="Lawu", variable=radio_var, value="Option 5", bg="yellow", fg="red",font=font_style)
+radio_button6 = tk.Radiobutton(frame, text="Sindoro", variable=radio_var, value="Option 6", bg="yellow", fg="red",font=font_style)
+radio_button7 = tk.Radiobutton(frame, text="Suket", variable=radio_var, value="Option 7", bg="yellow", fg="red",font=font_style)
+radio_button8 = tk.Radiobutton(frame, text="Suket", variable=radio_var, value="Option 8", bg="yellow", fg="red",font=font_style)
+radio_button9 = tk.Radiobutton(frame, text="Suket", variable=radio_var, value="Option 9", bg="yellow", fg="red",font=font_style)
+radio_button10 = tk.Radiobutton(frame, text="Suket", variable=radio_var, value="Option 10", bg="yellow", fg="red",font=font_style)
 
 # Menampilkan radio button
 radio_button1.pack(side="left")
 radio_button2.pack(side="left")
 radio_button3.pack(side="left")
 radio_button4.pack(side="left")
+radio_button5.pack(side="left")
+radio_button6.pack(side="left")
+radio_button7.pack(side="left")
+radio_button8.pack(side="left")
+radio_button9.pack(side="left")
+radio_button10.pack(side="left")
+
 
 # Membuat label untuk menampilkan gambar
 label = tk.Label(window, image=placeholder_image)
